4c77e7e48cc4d862866b38101ecf852d700dd541
Rupesh Chaudhari <rupesh.chaudhari05@gmail.com>
2026-01-22 22:34:15 +0530

    ,

--- database/migrations/015_add_due_day_to_savings_schemes.sql
+++ database/migrations/015_add_due_day_to_savings_schemes.sql
@@ -1,3 +1,3 @@
 -- Migration: Add due_day to savings_schemes (preferred day of month for dues)
 ALTER TABLE `savings_schemes`
-    ADD COLUMN `due_day` TINYINT UNSIGNED DEFAULT 1;
+ADD COLUMN `due_day` TINYINT UNSIGNED DEFAULT 1;
